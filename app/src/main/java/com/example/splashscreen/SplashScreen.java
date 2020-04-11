49ce3b63fd89924f96d941643140aa64bacc4359
Sakib <tanvirsakib370@gmail.com>
2020-04-11 22:15:22 +0600

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/SplashScreen.java
@@ -0,0 +1,56 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private int progress ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //to remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        progressBar = findViewById(R.id.progressBarID);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+                startApp();
+            }
+        });
+        thread.start();
+    }
+
+    public void doWork(){
+
+        for (progress = 10; progress<= 100; progress = progress+10 ){
+            try {
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void startApp(){
+        Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
